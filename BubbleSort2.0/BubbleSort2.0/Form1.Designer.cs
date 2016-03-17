c911003d43aba962a5b53b1041b507267bad40dc
Isaque de Araujo <isaquearaujo22@gmail.com>
2016-03-17 12:08:47 -0300

    SortFuncional
    Vesão Funcional

---
+++ BubbleSort2.0/BubbleSort2.0/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BubbleSort2._0
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.Tempo = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(329, 50);
+			this.chart1.Name = "chart1";
+			this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(409, 300);
+			this.chart1.TabIndex = 4;
+			this.chart1.Text = "chart1";
+			// 
+			// Tempo
+			// 
+			this.Tempo.AutoSize = true;
+			this.Tempo.Location = new System.Drawing.Point(504, 392);
+			this.Tempo.Name = "Tempo";
+			this.Tempo.Size = new System.Drawing.Size(35, 13);
+			this.Tempo.TabIndex = 6;
+			this.Tempo.Text = "label1";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(54, 105);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(837, 446);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.Tempo);
+			this.Controls.Add(this.chart1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label Tempo;
+		internal System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ BubbleSort2.0/BubbleSort2.0/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BubbleSort2._0
+{
+	public partial class Form1 : Form
+	{
+		Graphics g;
+		public static List<int> numeros = new List<int>();
+		int valor;
+		Stopwatch stopwatch = new Stopwatch();
+		TimeSpan time;
+		public static int contador;
+		Random random = new Random();
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+
+		static void BubbleSort(List<int> numeros)
+		{
+			for (int i = 0; i < numeros.Count; i++)
+			{
+				for (int k = i + 1; k < numeros.Count; k++)
+				{
+					if (numeros[i] > numeros[k])
+					{
+						int temp = numeros[i];
+						numeros[i] = numeros[k];
+						numeros[k] = temp;
+					}
+				}
+			}
+		}
+
+		static bool NoRepeat(int item)
+		{
+
+			if (!numeros.Contains(item))
+			{
+
+				numeros.Add(item);
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//listBox1.Items.Clear();
+			
+			contador = numeros.Count;
+			for (int i = 10; i < 2000; i+=5)
+			{
+				numeros.Clear();
+				for (int j = 0; j < i; j++)
+				{
+					numeros.Add(random.Next(0, 3000));
+				}
+				stopwatch.Restart();
+				BubbleSort(numeros);
+				stopwatch.Stop();
+				
+				chart1.Series[0].Points.AddXY(i, stopwatch.Elapsed.TotalMilliseconds);
+				Tempo.Text = time.ToString();
+			}
+		}
+	}
+}
